4a75d9d9b59a3a94fcf5e990815ed9fd9dfb5060
wfreeman2013 <a0507050697@gmail.com>
2025-10-27 13:39:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,6 @@ app = Flask(__name__)
 def home():
     return "Hello, Railway! 🚀"
 
-if _name_ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
